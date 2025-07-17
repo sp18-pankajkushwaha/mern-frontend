f6eeeaf8e2949078c59cb6c6f401b9adbb0b1d8b
Pankaj Kushwaha <pankaj.kushwaha@sparkeighteen.com>
2025-07-17 11:17:23 +0530

    Delete HTML/50_days_50_projects directory

--- HTML/50_days_50_projects/day3/script.js
+++
@@ -1,6 +0,0 @@
-const search=document.querySelector('.container')
-const input=document.querySelector('.input')
-search.addEventListener('click', ()=>{
-    search.classList.toggle('active')
-    input.focus();
-})
